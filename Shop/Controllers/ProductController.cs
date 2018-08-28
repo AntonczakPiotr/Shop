59c6759b8f732a9a3e5f4849c20012ab3863f976
Piotr Antończak <AntonczakPiotr@gmail.com>
2018-08-28 15:51:38 +0200

    Shop. do rozdziału 2.7. (MVC, przekazywanie modelu do widoku)

---
+++ Shop/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            Product product = new Product();
+            product.Name = "Książka ASP.NET MVC";
+            product.Description = "Znajdziemy tutaj bardzo dużo wiedzy!";
+            product.Price = 100;
+
+            return View(product);
+        }
+    }
+}
